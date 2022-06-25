e89c9cc7fdec983d3ec6f4f484dd1ded687ea360
itamar <itamarcu@gmail.com>
2022-06-25 20:11:45 +0300

    Added confirmation prompt

--- scripts/player-token-delete.js
+++ scripts/player-token-delete.js
@@ -23,19 +23,51 @@ export function requestDelete(ids) {
   if (game.user.isGM) {
     canvas.scene.deleteEmbeddedDocuments('Token', tokens);
   }
-  // If not gm, request deletion via socket
   else {
-    // Request GM user to delete tokens
-    game.socket.emit(`module.${APTDTT}`, {
-      op: 'delete',
-      user: game.user.id,
-      scene: canvas.scene.id,
-      tokens,
-    });
-    // eslint-disable-next-line no-console
-    console.log(`${APTDTT} | Requesting GM delete tokens ${JSON.stringify(tokens)} from scene ${canvas.scene.id}`);
+    // If not gm, request deletion via socket, after confirmation
+    const emitViaSocket = () => {
+      // Request GM user to delete tokens
+      game.socket.emit(`module.${APTDTT}`, {
+        op: 'delete',
+        user: game.user.id,
+        scene: canvas.scene.id,
+        tokens,
+      });
+      // eslint-disable-next-line no-console
+      console.log(`${APTDTT} | Requesting GM delete tokens ${JSON.stringify(tokens)} from scene ${canvas.scene.id}`);
+    };
+    if (game.keyboard.isModifierActive(KeyboardManager.MODIFIER_KEYS.SHIFT)) {
+      emitViaSocket();
+    }
+    else {
+      promptConfirmation(tokens, emitViaSocket);
+    }
   }
 }
+
+const promptConfirmation = (tokens, emitViaSocket) => {
+  const pluralS = tokens.length === 1 ? '' : 's';
+  new Dialog({
+    title: `Delete your tokens?`,
+    content: `<p>Are you sure you want to delete your token${pluralS}?</p>
+<p>Token name${pluralS}: ${tokens.map(t => t.name)
+      .join(', ')}</p>
+<p>(You can hold Shift to skip this confirmation prompt)</p>`,
+    buttons: {
+      ok: {
+        label: `Delete token${pluralS}`,
+        icon: `<i class="fas fa-check"></i>`,
+        callback: emitViaSocket,
+      },
+      cancel: {
+        label: 'Cancel',
+        icon: `<i class="fas fa-times"></i>`,
+        callback: () => {},
+      },
+    },
+    default: 'ok',
+  }).render(true);
+};
 
 /*
  * React to keyboard events and if delete requested, process and emit socket event
